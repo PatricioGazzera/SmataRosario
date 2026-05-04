234c69613295bb51e0ef9277ebd594dd6fdb4503
Aportes <patriciogazzera7@gmail.com>
2026-05-04 12:17:49 -0300

    fix: force case sensitive routes import

--- smataRosario/src/App.jsx
+++ smataRosario/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Footer from './Components/Footer/Footer.jsx'
 import Navbar from './Components/Navbar/Navbar.jsx'
-import AppRoutes from './Routes/AppRoutes.jsx'
+import AppRoutes from './routes/AppRoutes.jsx'
 import ScrollToTop from './Components/ScrollToTop.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'

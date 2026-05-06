c4fc1d236c6962992f66c68b9f7aed1be387f054
Aportes <patriciogazzera7@gmail.com>
2026-05-06 08:33:57 -0300

    fix

--- smataRosario/src/main.jsx
+++ smataRosario/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
-import { Analytics } from "@vercel/analytics/react"
+import { inject } from "@vercel/analytics"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+inject();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 )

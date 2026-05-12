6a7482b0345d34526bf2c6fb028b53171d7036c6
Aportes <patriciogazzera7@gmail.com>
2026-05-12 10:20:43 -0300

    paquete vercel

--- smataRosario/src/main.jsx
+++ smataRosario/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { inject } from "@vercel/analytics"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,5 +9,6 @@ inject();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
